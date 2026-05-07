c09bacc353e872309eb31afb8099a97d2cb0c8dc
wede-CMYK <3089978173@qq.com>
2026-05-07 22:47:43 +0800

    first try

--- src/App.java
+++ src/App.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args){
-        //启动类
-        
+        //启动类\
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        System.out.println(a);
+        Integer b;
+
 
 
     }
